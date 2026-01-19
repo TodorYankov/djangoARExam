090701b3471a61012f923157101a199aa54e3762
Todor Yankov <webdesign3@abv.bg>
2026-01-19 14:56:26 +0200

    Aктуализиране на Django app конфигурации след преструктуриране

--- orders/apps.py
+++ orders/apps.py
@@ -1,5 +1,7 @@
 from django.apps import AppConfig
 
+class OrdersConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'orders'  # ← с пълния път!
 
-class AccountsConfig(AppConfig):
-    name = 'accounts'
+
