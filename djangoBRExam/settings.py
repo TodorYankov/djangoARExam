5a1e4afe86d915a5425e7ed5de8d9b7166d03bc1
Todor Yankov <webdesign3@abv.bg>
2026-01-23 20:34:05 +0200

    Configure Django settings for future global templates and static files

--- djangoBRExam/settings.py
+++ djangoBRExam/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # My apps
     'core',
     'products',
     'orders',
@@ -57,7 +59,7 @@ ROOT_URLCONF = 'djangoBRExam.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],  # Променено!
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
