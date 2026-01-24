aafdf5144fe413b3cfae7fc7d201486786e49c32
Todor Yankov <webdesign3@abv.bg>
2026-01-24 19:00:58 +0200

    Обновени URL шаблони в djangoBRExam/urls.py

--- djangoBRExam/urls.py
+++ djangoBRExam/urls.py
@@ -15,8 +15,23 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.urls import path, include
+from django.conf.urls.static import static
+from core.views import home_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home_view, name='home'),
+    path('products/', include('products.urls')),
+    path('orders/', include('orders.urls')),
+    path('core/', include('core.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+# Персонализирана 404 страница
+handler404 = 'core.views.custom_404'
